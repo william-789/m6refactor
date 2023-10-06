f64ee4d5f1c2d3feb1de9b4b7456567171439678
William Rodrigues <riberackw@gmail.com>
2023-10-06 16:18:07 +0100

    feat: correctly filters the result

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,9 @@
 import "./SearchBar.scss"
 import searchIcon from "../../assets/icons/nav_search.svg";
+
+
 function SearchBar({setFilter}) {
+ 
   return (
     <div className="search-bar bar">
       <div className={"search-box"}>
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -5,32 +5,49 @@ import ListItem from "../../components/List_Item/ListItem";
 import { useState, useEffect } from "react";
 function Search() {
   const [events, setEvents] = useState(null)
-  const [artists,setArtists] = useState(null)
+  const [artists, setArtists] = useState(null)
   const [eventsNext, setEventsNext] = useState([])
-  const [artistsNext,setArtistsNext] = useState([])
+  const [artistsNext, setArtistsNext] = useState([])
   const [page, setPage] = useState(0)
-  const [filtro,setFiltro] = useState("")
+  const [filtro, setFiltro] = useState("")
+  const [searchResults, setSearchResults] = useState([])
 
-  useEffect(() => {
-    axiosFest.get("/artistas/listar", {params:{numero_resultados:filtro ? 3 : 4, pagina: page}}).then((res)=> {
-      console.log(res.data.artistas)
-      setArtists(res.data.artistas)
+  function typeData(data, type) {
+    return data.map((el) => {
+      return {
+        ...el,
+      type
+      }
     })
-    axiosFest.get("/evento/listar", {params:{numero_resultados:3, pagina: page}}).then((res)=> {
-      console.log(res.data)
-      setEvents(res.data.eventos)
+  }
+
+  const getData = async () => {
+    const combinedRes = []
+    await axiosFest.get("/artistas/listar", {params:{numero_resultados:filtro ? 3 : 4, pagina: page, pesquisa: filtro}}).then((res)=> {
+      const typedRes = typeData(res.data.artistas, "artist")
+      combinedRes.push(...typedRes)
+      setArtists(typedRes)
     })
+    await axiosFest.get("/evento/listar", {params:{numero_resultados:3, pagina: page, pesquisa: filtro}}).then((res)=> {
+      const typedRes = typeData(res.data.eventos, "event")
+      combinedRes.push(...typedRes)
+      setEvents(typeData(res.data.eventos, "event"))
+    })
+
+    setSearchResults(combinedRes)
 
     if(filtro) {
-      axiosFest.get("/artistas/listar", {params:{numero_resultados:3, pagina: page + 1}}).then((res)=> {
-        console.log(res.data.artistas)
-        setArtistsNext(res.data.artistas)
+      await axiosFest.get("/artistas/listar", {params:{numero_resultados:3, pagina: page + 1, pesquisa: filtro}}).then((res)=> {
+        setArtistsNext(typeData(res.data.artistas, "artist"))
       })
-      axiosFest.get("/evento/listar", {params:{numero_resultados:3, pagina: page + 1}}).then((res)=> {
-        console.log(res.data)
-        setEventsNext(res.data.eventos)
+      await axiosFest.get("/evento/listar", {params:{numero_resultados:3, pagina: page + 1, pesquisa: filtro}}).then((res)=> {
+        setEventsNext(typeData(res.data.eventos, "event"))
       })
     }
+  }
+
+  useEffect(() => {
+    getData()
   },[filtro, page])
 
   // page navigation controller
@@ -43,25 +60,38 @@ function Search() {
     )
   }
 
+  // return of first load and when filter is empty
   return (
     <div className={"Search page"}>
       <SearchBar setFilter={setFiltro} />
       {!filtro && <h1>Festivais</h1>}
-      {
-        events.length === 0 ?
+      {!filtro &&
+        (events.length === 0 ? (
           <p>Sem eventos encontrados.</p>
-          :
-        events.map((e) => <ListItem key={e.id} imagem={e.imagem} title={e.designacao} line2={e.local} />)
-      }
+        ) : (
+          events.map((e) => (
+            <ListItem key={e.id+"e"} imagem={e.imagem} title={e.designacao} line2={e.local} />
+          ))
+        ))}
       {!filtro && <h1>Artistas</h1>}
-      {
-        artists.length === 0 ?
+      {!filtro &&
+        (artists.length === 0 ? (
           <p>Sem artistas encontrados.</p>
-          :
-        artists.map((a) => <ListItem key={a.id} round={true} imagem={a.imagem} title={a.nome} line2={"Artista"} />)
-      }
+        ) : (
+          artists.map((a) => (
+            <ListItem key={a.id+"a"} round={true} imagem={a.imagem} title={a.nome} line2={"Artista"} />
+          ))
+        ))}
+  
+      {filtro &&
+        searchResults.map((r) => {
+          if (r.type === "event") return <ListItem key={r.id+"e"} imagem={r.imagem} title={r.designacao} line2={r.local} />;
+          return <ListItem key={r.id+"a"} round={true} imagem={r.imagem} title={r.nome} line2={"Artista"} />;
+        })}
+      {filtro && searchResults.length === 0 && <p>Sem resultados encontrados.</p>}
     </div>
-  )
+  );
+  
 }
 
 export default Search
