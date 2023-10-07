0635c471bbf8efa7474f8c405a372fe046862b81
William Rodrigues <riberackw@gmail.com>
2023-10-07 12:35:24 +0100

    feat: keep track of favorites locally

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -1,15 +1,42 @@
-import React from "react";
-import heartOpen from "../../assets/icons/heart_open.svg";
-import heartFull from "../../assets/icons/heart_full.svg";
+import React, {useContext, useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartFull, faInfo } from "@fortawesome/free-solid-svg-icons";
+import ticket from "../../assets/icons/nav_ticket_active.svg";
+import {UserContext} from "../../context/UserContext";
+import axiosfest from "../../services/axiosfest";
 
-function Like({ liked = false }) {
-  return (
-    <img
-      src={liked ? heartFull : heartOpen}
-      alt={liked ? 'liked' : 'like'}
-      className='side-info like'
-    />
-  );
+function Like({ type = "like", liked = false, favType, favEl }) {
+  const { userData, toggleFavLocal } = useContext(UserContext)
+  const { email } = userData
+  const [isLiked, setIsLiked] = useState(liked);
+
+  const toggleFav = async () => {
+    const favPath = favType === "event" ? 'toggle_evento' : 'toggle_artista'
+    const fav = favType === "event" ? 'evento' : 'artista'
+    await axiosfest.post(`/participante/favoritos/${favPath}`, {
+      participante: email,
+      [fav]: favEl.id
+    })
+    toggleFavLocal(favEl)
+    setIsLiked(!isLiked)
+  }
+
+  if (type === "info") {
+    return <FontAwesomeIcon icon={faInfo} className="side-info like" />;
+  } else if (type === "ticket") {
+    return (
+        <img
+            src={ticket}
+            alt=""
+            className="side-info like ticket"
+        />
+    );
+  } else {
+    const icon = isLiked ? faHeartFull : faHeart;
+    return <FontAwesomeIcon icon={icon} className="side-info like" onClick={()=>{toggleFav()}}/>;
+  }
 }
 
 export default Like;
+
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -4,12 +4,26 @@ const UserContext = React.createContext({});
 
 function UserProvider (props) {
   const [userData, setUserData] = useState({}) // {name: , email:}
+  const [favorites, setFavorites] = useState([])
 
   function isLogged() {
     return !!(userData.name && userData.email)
   }
 
-  return <UserContext.Provider value={{userData,setUserData, isLogged}}>
+  function isFavorite(el) {
+    return favorites.includes(el)
+  }
+
+  function toggleFavLocal(el) {
+    if(isFavorite(el)) {
+      const listWithoutEl = favorites.filter((favEl) => favEl !== el)
+      setFavorites(listWithoutEl)
+    } else {
+      setFavorites([...favorites, el])
+    }
+  }
+
+  return <UserContext.Provider value={{userData,setUserData, isLogged, isFavorite, toggleFavLocal, setFavorites}}>
     {props.children}
   </UserContext.Provider>
 }
